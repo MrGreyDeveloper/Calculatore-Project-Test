dd320e855ac928aab296204506c8a83ce1d16e35
MrGreyDeveloper <bukvic96@gmail.com>
2024-06-22 12:59:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
-import Button from "./components/Button/Button";
-import Display from "./components/Display/Display";
-import ClearButton from "./components/ClearButton/ClearButton";
-import DeleteButton from "./components/DeleteButton/DeleteButton";
-import EqualsButton from "./components/EqualsButton/EqualsButton";
+import Button from "@mui/material/Button";
+import Display from "./components/Display/Display.js";
+import ClearButton from "./components/ClearButton/ClearButton.js";
+import DeleteButton from "./components/DeleteButton/DeleteButton.js";
 import { evaluateExpression } from "./utils";
 
 function App() {
   const [value, setValue] = useState("");
 
   const handleClick = (e) => {
-    setValue(value + e.target.value);
+    setValue(value + e.target.textContent);
   };
 
   const handleEqualClick = () => {
@@ -32,7 +31,9 @@ function App() {
 
   const renderButtons = (values, onClick) => {
     return values.map((val) => (
-      <Button key={val} value={val} onClick={onClick} />
+      <Button key={val} variant="contained" onClick={onClick}>
+        {val}
+      </Button>
     ));
   };
 
@@ -44,19 +45,25 @@ function App() {
           <div>
             <ClearButton onClick={handleClearClick} />
             <DeleteButton onClick={handleDeleteClick} />
-            <Button value="." onClick={handleClick} />
-            <Button value="/" onClick={handleClick} />
+            <Button variant="contained" onClick={handleClick}>
+              .
+            </Button>
+            <Button variant="contained" onClick={handleClick}>
+              /
+            </Button>
           </div>
-
           <div>{renderButtons(["7", "8", "9", "*"], handleClick)}</div>
-
           <div>{renderButtons(["4", "5", "6", "+"], handleClick)}</div>
-
           <div>{renderButtons(["1", "2", "3", "-"], handleClick)}</div>
-
           <div>
             {renderButtons(["00", "0"], handleClick)}
-            <EqualsButton onClick={handleEqualClick} className="equal" />
+            <Button
+              variant="contained"
+              onClick={handleEqualClick}
+              className="equal"
+            >
+              =
+            </Button>
           </div>
         </form>
       </div>
